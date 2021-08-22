37c68e61cdf5e1fa732c858cb7eeeafbb6f04d57
marcia <88407854+mar3981@users.noreply.github.com>
2021-08-21 22:49:46 -0300

    Add files via upload

--- homework.js
+++ homework.js
@@ -1,329 +1,251 @@
-// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
-
-// Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "marcia";
-
-// Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 39;
-
-// Crea una variable booleana:
-const nuevoBool = false;
-
-// Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5 === 5;
-
-// Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * 4 === 40 ;
-
-// Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1;
-
-
-// En los próximos 22 problemas, deberás completar la función.
-// Todo tu código irá dentro de las llaves de la función.
-// Asegúrate que usas "return" cuando la consola te lo pida.
-// Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-function devolverString(str) {
-  // "Return" la string provista: str
+function obtenerMayor(x, y) {
+  // "x" e "y" son números enteros (int).
+  // Devuelve el número más grande
+  // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  var nombreDeRata = str;
-  return nombreDeRata;
+  if(x > y) {
+    return x;
+  }
+  return y;
 }
-devolverString("totoro")
+obtenerMayor(2,4) // 4
 
+function mayoriaDeEdad(edad) {
+  //Determinar si la persona según su edad puede ingresar a un evento.
+  //Si tiene 18 años ó más, devolver --> "Allowed"
+  //Si es menor, devolver --> "Not allowed"
+  if(edad >= 18) {
+    return "Allowed";
+  }
+   return "Not allowed";
+}
+mayoriaDeEdad(22) // "Allowed"
+  
+function conection(status) {
+  //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
+  //Cuando el estado es igual a 1, el usuario está "Online"
+  //Cuando el estado es igual a 2, el usuario está "Away"
+  //De lo contrario, presumimos que el usuario está "Offline"
+  //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if(status === 1) {
+    return "Online";
+  } else if( status === 2) {
+    return "Away";
+  }
+  return "Offline";
+}
+conection(1) // "Online"
 
-function suma(x, y) {
-  // "x" e "y" son números
-  // Suma "x" e "y" juntos y devuelve el valor
+function saludo(idioma) {
+  // Devuelve un saludo en tres diferentes lenguajes:
+  // Si "idioma" es "aleman", devuelve "Guten Tag!"
+  // Si "idioma" es "mandarin", devuelve "Ni Hao!"
+  // Si "idioma" es "ingles", devuelve "Hello!"
+  // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  var suma = x + y;
-  return suma;
+  if(idioma === "aleman") {
+    return "Guten Tag!";
+  } else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } else if(idioma === "ingles") {
+    return "Hello!";
+  }
+  return "Hola!"
 }
-suma(12,7) // 19
+saludo("aleman") // "Guten Tag!"
 
-function resta(x, y) {
-  // Resta "y" de "x" y devuelve el valor
-  // Tu código:
-  var resta = x - y;
-  return resta;
+function colors(color) {
+  //La función recibe un color. Devolver el string correspondiente:
+  //En caso que el color recibido sea "blue", devuleve --> "This is blue"
+  //En caso que el color recibido sea "red", devuleve --> "This is red"
+  //En caso que el color recibido sea "green", devuleve --> "This is green"
+  //En caso que el color recibido sea "orange", devuleve --> "This is orange"
+  //Caso default: devuelve --> "Color not found"
+  //Usar el statement Switch.
+   switch(color) {
+    case "blue":
+      return "This is blue";
+    case "red": 
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default: 
+    return "Color not found";
+  }
+     
 }
-resta(57,-30) // 87
+colors("blue") // "This is blue"
 
-function multiplica(x, y) {
-  // Multiplica "x" por "y" y devuelve el valor
-  // Tu código:
-  var multiplica = x * y;
-  return multiplica;
-}
-multiplica(3,100) // 300
-
-function divide(x, y) {
-  // Divide "x" entre "y" y devuelve el valor
-  // Tu código:
-  var divide = x / y;
-  return divide;
-}
-divide(81,9) // 9
-
-function sonIguales(x, y) {
-  // Devuelve "true" si "x" e "y" son iguales
+function esDiezOCinco(numero) {
+  // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-   if(x === y) {
-    return true;
-   }  
-   return false;
+  return numero ===10 || numero === 5
+  
 }
-sonIguales(2,2) // true
+esDiezOCinco(6) // false
 
-function tienenMismaLongitud(str1, str2) {
-  // Devuelve "true" si las dos strings tienen la misma longitud
+function estaEnRango(numero) {
+  // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-     return str1.length === str2.length 
-
-   }
- tienenMismaLongitud("java","one") // false
-
-    
-function menosQueNoventa(num) {
-  // Devuelve "true" si el argumento de la función "num" es menor que noventa
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  if(num < 90) {
-    return true;
-  } 
-  return false;
-}
-menosQueNoventa(245) // false 
-
-function mayorQueCincuenta(num) {
-  // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  if(num > 50){
+  if(numero < 50 && numero >20) {
     return true;
   }
-  return false;
+   return false;
 }
-mayorQueCincuenta(51) // true
+estaEnRango(43) // true
 
-function obtenerResto(x, y) {
-  // Obten el resto de la división de "x" entre "y"
-  // Tu código:
-  var resto = x % y;
-  return resto;
-}
-obtenerResto(75,7) // 5
-
-function esPar(num) {
-  // Devuelve "true" si "num" es par
+function esEntero(numero) {
+  // Devuelve "true" si "numero" es un entero (int/integer)
+  // Ejemplo: 0.8 -> false
+  // Ejemplo: 1 -> true
+  // Ejemplo: -10 -> true
   // De lo contrario, devuelve "false"
+  // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if(num % 2 === 0) {
+  if(numero % 1 === 0) {
+    return true;
+  } else if(numero % 1 !== 0);
+  return false;
+  return Math.floor;
+}
+esEntero(2) // true
+
+function fizzBuzz(numero) {
+  // Si "numero" es divisible entre 3, devuelve "fizz"
+  // Si "numero" es divisible entre 5, devuelve "buzz"
+  // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
+  // De lo contrario, devuelve el numero
+  if(numero % 15 === 0) {
+    return "fizzbuzz";
+  }
+  if(numero % 3 === 0) {
+    return "fizz";
+  } 
+  if(numero % 5 === 0) {
+    return "buzz";
+  }  
+  if(numero % 3 !== 0 && numero % 5 !== 0) {
+  return numero;
+  }
+}
+fizzBuzz(30) // "fizzbuzz"
+
+function operadoresLogicos(num1, num2, num3) {
+  //La función recibe tres números distintos. 
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+   if(num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  else {
+    return false;
+  }
+}
+operadoresLogicos(-2) // "Hay negativos"
+
+function esPrimo(numero) {
+  // Devuelve "true" si "numero" es primo
+  // De lo contrario devuelve "falso"
+  // Pista: un número primo solo es divisible por sí mismo y por 1
+  // Pista 2: Puedes resolverlo usando un bucle `for`
+  // Nota: Los números 0 y 1 NO son considerados números primos
+  if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+esPrimo(3) // "true"
+
+function esVerdadero(valor){
+  //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
+  //si su valor es true y “Soy falso” si su valor es false.
+  //Escribe tu código aquí
+  if(valor === true) {
+    return "Soy verdadero";
+  }
+  if( valor === false) {
+    return "Soy falso"
+  }
+}
+esVerdadero(45) // "Soy verdadero"
+
+function tablaDelSeis(){
+  //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
+  //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
+  //Escribe tu código aquí 
+  let  arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
+  }
+  return arrayTablaDel6
+}
+tablaDelSeis(0,6,12,18,24,30,36,42,48,54,60) // return arrayTabladel6
+
+function tieneTresDigitos(numero){
+  //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
+  //Escribe tu código aquí
+  if(numero >99 && numero < 1000) {
     return true;
   }
     return false;
+}
+tieneTresDigitos(1500) // false
+
+function doWhile(numero) {
+  //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
+  //Retornar el valor final.
+  //Usar el bucle do ... while.
+  var x = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    x = x + 5;
   }
-  esPar(4) // true
-  
-esPar(4) // true 
-function esImpar(num) {
-  // Devuelve "true" si "num" es impar
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  if(num % 2 ===1) {
-    return true;
-  }
-  return false;
+  while( i < 8);
+  return x;
 }
-esImpar(13) // true
-
-function elevarAlCuadrado(num) {
-  // Devuelve el valor de "num" elevado al cuadrado
-  // ojo: No es raiz cuadrada!
-  // Tu código:
-  return Math.pow(num,2);
-  
-}
-elevarAlCuadrado(2) // 4 
-
-
-function elevarAlCubo(num) {
-  // Devuelve el valor de "num" elevado al cubo
-  // Tu código:
-  return Math.pow(num,3);
-}
-elevarAlCubo(1) // 1
-
-function elevar(num, exponent) {
-  // Devuelve el valor de "num" elevado al exponente dado en "exponent"
-  // Tu código:
- return Math.pow(num,exponent);
-}
-elevar(2,5) // 32
-
-function redondearNumero(num) {
-  // Redondea "num" al entero más próximo y devuélvelo
-  // Tu código:
-  return Math.round(num);
-}
-redondearNumero(5.2) // 5
-
-function redondearHaciaArriba(num) {
-  // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
-  // Tu código:
-  return Math.ceil(num);
-}
-redondearHaciaArriba(2.1) // 3
-
-function numeroRandom() {
-  //Generar un número al azar entre 0 y 1 y devolverlo
-  //Pista: investigá qué hace el método Math.random()
-  return Math.random();
-}
-numeroRandom(0.13142567) // 
-
-function esPositivo(numero) {
-  //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
-  //Si el número es positivo, devolver ---> "Es positivo"
-  //Si el número es negativo, devolver ---> "Es negativo"
-  //Si el número es 0, devuelve false
-  if(numero > 0) {
-  return"Es positivo";
-  }
-  if(numero < 0) {
-  return "Es negativo";
-  }
-  if(numero = 0);
-  return false;
-}
-esPositivo(0) // false 
-  
-
-function agregarSimboloExclamacion(str) {
-  // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
-  // Ejemplo: "hello world" pasaría a ser "hello world!"
-  // Tu código:
-  return str + "!";
-}
-
-agregarSimboloExclamacion("Hello world!") //
-
-
-function combinarNombres(nombre, apellido) {
-  // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
-  // Ejemplo: "Soy", "Henry" -> "Soy Henry"
-  // Tu código:
-  return nombre + " " + apellido;
-
-  
-}
-combinarNombres( "Marcia Escobar") 
-
-function obtenerSaludo(nombre) {
-  // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
-  // "Martin" -> "Hola Martin!"
-  // Tu código:
- var nombre = "Hola " + nombre + "!";
- return nombre
-
-}
-obtenerSaludo( "Hola Martin!") // 
-
-function obtenerAreaRectangulo(alto, ancho) {
-  // Retornar el area de un rectángulo teniendo su altura y ancho
-  // Tu código:
-  return alto * ancho;
-}
-obtenerAreaRectangulo(29,12) // 348
-
-
-function retornarPerimetro(lado){
-  //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
-  //Escribe tu código aquí
-  var perimetro = lado * 4;
-  return perimetro;
-}
-retornarPerimetro(3.5) // 14
-
-
-function areaDelTriangulo(base, altura){
-  //Desarrolle una función que calcule el área de un triángulo.
-  //Escribe tu código aquí
-  var area = (base * altura)/ 2;
-  return area;
-
-}
-areaDelTriangulo(2.7,4) // 5.4
-
-
-function deEuroAdolar(euro){
-  //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
-  //como parámetro un número de euros y calcule el cambio en dólares.
-  //Escribe tu código aquí
-  var cambio = euro * 1.20;
-  return cambio;
-  
-}
-deEuroAdolar(100) // 120 dolares
-
-function esVocal(letra){
-  //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
-  //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
-  //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
-  //Escribe tu código aquí
-  if(letra.length > 1){
-    return "Dato incorrecto"
-  }
-  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
-    return "Es vocal"
-  }
-  return "Dato incorrecto"
-
-}
-
-esVocal("la") // "Dato incorrecto"
-
+doWhile(40)
 
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-  nuevaString,
-  nuevoNum,
-  nuevoBool,
-  nuevaResta,
-  nuevaMultiplicacion,
-  nuevoModulo,
-  devolverString,
-  tienenMismaLongitud,
-  sonIguales,
-  menosQueNoventa,
-  mayorQueCincuenta,
-  suma,
-  resta,
-  divide,
-  multiplica,
-  obtenerResto,
-  esPar,
-  esImpar,
-  elevarAlCuadrado,
-  elevarAlCubo,
-  elevar,
-  redondearNumero,
-  redondearHaciaArriba,
-  numeroRandom,
-  esPositivo,
-  agregarSimboloExclamacion,
-  combinarNombres,
-  obtenerSaludo,
-  obtenerAreaRectangulo,
-  retornarPerimetro,
-  areaDelTriangulo,
-  deEuroAdolar,
-  esVocal,
+  obtenerMayor,
+  mayoriaDeEdad,
+  conection,
+  saludo,
+  colors,
+  esDiezOCinco,
+  estaEnRango,
+  esEntero,
+  fizzBuzz,
+  operadoresLogicos,
+  esPrimo,
+  esVerdadero,
+  tablaDelSeis,
+  tieneTresDigitos,
+  doWhile
 };
